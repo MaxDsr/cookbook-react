99171dae8ce9c212289814ff0b2cab4287df324f
maximdicusari <maxim.dicusari@gmail.com>
2025-08-12 14:59:40 +0300

    - update auth

--- backend/src/constants/auth0Config.ts
+++ backend/src/constants/auth0Config.ts
@@ -1,6 +1,6 @@
 
 export const auth0Config = {
-  "domain": "dev-42ovpq2mja2h0nqq.us.auth0.com",
-  "clientId": "osfej91vbYVhyiJaI8bLqYSdwnBbAWvQ",
-  "audience": "https://dev-42ovpq2mja2h0nqq.us.auth0.com/api/v2/"
+  "domain": "dev-q2n82wdt7tl0hbnm.eu.auth0.com",
+  "clientId": "Phq0OjvqmLysJi6b2gARzxf7IwMPv57C",
+  "audience": "https://dev-q2n82wdt7tl0hbnm.eu.auth0.com/api/v2/"
 };
--- frontend/src/auth0Config.js
+++ frontend/src/auth0Config.js
@@ -5,10 +5,10 @@ const history = createBrowserHistory();
 
 
 export const auth0Config = {
-  "domain": "dev-42ovpq2mja2h0nqq.us.auth0.com",
-  "clientId": "osfej91vbYVhyiJaI8bLqYSdwnBbAWvQ",
+  "domain": "dev-q2n82wdt7tl0hbnm.eu.auth0.com",
+  "clientId": "Phq0OjvqmLysJi6b2gARzxf7IwMPv57C",
   authorizationParams: {
-    "audience": "https://dev-42ovpq2mja2h0nqq.us.auth0.com/api/v2/",
+    "audience": "https://dev-q2n82wdt7tl0hbnm.eu.auth0.com/api/v2/",
     redirect_uri: window.location.origin,
   },
   onRedirectCallback: (appState) => {
